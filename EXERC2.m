000f386756c3b3db44e9d13163914f949ad03b75
Jeferson <jeferson.silva220@gmail.com>
2025-02-18 21:32:23 -0300

    Add files via upload

---
+++ EXERC2.m
@@ -0,0 +1,27 @@
+#ones
+A = ones(2,1) # 2 linhas, 1 coluna
+B = ones(3,4) # 3 linhas, 4 colunas
+
+#zeros
+C = zeros(2,2) %cria vetor nulo
+C = zeros(4,4)
+
+#rand = randômico = aleatório
+D = rand(2,2)
+
+#concatenar vetores
+E = [A,A]
+
+#verificar o maior valor
+
+maiorvalor = max(D)
+
+menorvalor = min(D)
+
+
+
+
+
+
+
+
---
+++ EXERC4.m
@@ -0,0 +1,7 @@
+function y = funcoes(x)
+  funcoes = x.^2 + 1;
+endfunction
+
+ resultado = funcoes(1);
+ disp(['O resultado da função é:',num2str(resultado)])
+
